b0ebf452ff60db4373bc14567395cc49ff34253a
roman <roman_chetin@mail.ru>
2022-04-11 23:47:21 +0700

    Update settings.py

--- places_remember_back/settings/settings.py
+++ places_remember_back/settings/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': 'places_remember_db',
         'USER': 'places_remember_user',
         'PASSWORD': '123',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
